d7a0321f7b3f93efa9a741b20849e94ddb4ef347
rim <sandrochia@naver.com>
2020-03-06 20:23:14 +0900

    Profile Controller, Test code 추가

---
+++ src/test/java/com/metamong/book/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,68 @@
+package com.metamong.book.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+
+    @Test
+    public void real_profile_check() {
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    /**
+     * real profile이 없으면 첫 번째가 조회된다.
+     */
+    @Test
+    public void first_profile_is_inquired() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    /**
+     * active profile이 없으면 default가 조회된다.
+     */
+    @Test
+    public void default_profile_is_inquired() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
+
+
